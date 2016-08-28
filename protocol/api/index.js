e498f6e6ab4939f8cfb7331a5f140a87c897e625
Brian Spates <brian.spates@moz.com>
2016-08-28 11:29:21 -0700

    update interface with codecs

--- protocol/api/index.js
+++ protocol/api/index.js
@@ -1,3 +1,8 @@
+
+/**
+ * Encoding/Decoding of binary Kafka API messages as defined on:
+ * http://kafka.apache.org/protocol.html#protocol_messages
+ */
 module.exports = {
   Metadata: require('./metadata'),
   Header: require('./header')
--- protocol/index.js
+++ protocol/index.js
@@ -1,18 +1,5 @@
-var definitions = require('./definitions');
-var types = require('./types');
-var api = require('./api');
-
-/**
- * Encoding/Decoding of binary Kafka API messages as defined on:
- * http://kafka.apache.org/protocol.html#protocol_messages
- */
-module.exports = class Protocol {
-
-  static decodeMetadata(buffer) {
-    return api.Metadata.decodeResponse(buffer);
-  }
-
-  static encodeMetadata(correlationId, clientId, topics) {
-    return api.Metadata.encodeRequest(correlationId, clientId, topics);
-  }
+module.exports = {
+  api: require('./api'),
+  types: require('./types'),
+  definitions: require('./definitions')
 };
