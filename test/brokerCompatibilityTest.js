22ae1f8d02e170e5060c5d8f4cc1f07c427e97ac
Brian Spates <brian.spates@moz.com>
2016-11-28 10:38:48 -0800

    add unknown message test

--- test/brokerCompatibilityTest.js
+++ test/brokerCompatibilityTest.js
@@ -69,8 +69,52 @@ describe('When using the API -> ', () => {
   });
 
   describe('Produce', () => {
-    it('should publish message to topic', () => {
-      
+    it('should recieve UNKNOWN_TOPIC error when publishing to new topic', () => {
+      client.produce([
+        {
+          topic: 'nope',
+          data: [
+            {
+              partition: 0,
+              recordSet: [
+                {
+                  value: "0"
+                },
+                {
+                  value: "1"
+                },
+                {
+                  value: "2"
+                }
+              ]
+            }
+          ]
+        }
+      ], (err, result) => {
+        if(err) return done(err);
+        assert.deepEqual(result, {
+          "responses": [
+            {
+              "topic": "woot",
+              "partitionResponses": [
+                {
+                  "partition": 0,
+                  "error": {
+                    "name": "UNKNOWN_TOPIC_OR_PARTITION",
+                    "code": 3,
+                    "retry": true,
+                    "message": "This server does not host this topic-partition."
+                  },
+                  "offset": -1,
+                  "timestamp": -1
+                }
+              ]
+            }
+          ],
+          "throttleTime": 0
+        });
+        done();
+      });
     });
   });
 });
